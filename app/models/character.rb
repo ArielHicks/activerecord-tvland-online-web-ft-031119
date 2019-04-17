7307393d1d45bbc85f48a079f60428c82312e2be
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-17 18:18:14 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+  belongs_to :network
+
+
+  def self.say_that_thing_you_say
+    catchphrase = "#{self.name} always says: #{self.catchphrase}"
+    catchphrase
+    # Define a method in the Character class, #say_that_thing_you_say,
+    # using a given character's catchphrase.
+  end
 end
